780de19559c5186ef0b23a5316176362673ceb57
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-10-16 11:12:30 -0300

    chore: // nolint:staticcheck

--- cmd/commands/get.go
+++ cmd/commands/get.go
@@ -17,7 +17,7 @@ var GetCmd = &cobra.Command{
 }
 
 func getVal(cmd *cobra.Command, args []string) {
-	//lint:ignore SA9003 ignore this!
+	// nolint:staticcheck
 	if err := database.DB.Update(
 		func(tx *nutsdb.Tx) error {
 			key := []byte(args[0])
