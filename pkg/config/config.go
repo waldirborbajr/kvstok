291842a9eed7b5d7f9ae020121850a0ee0cbe51b
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-10-18 17:33:03 -0300

    chore: ajust to version on dev environment

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -4,7 +4,7 @@ package config
 // go build -v -ldflags="-X 'github.com/waldirborbajr/kvstok/cmd.Verzion=v2.0.0'"
 // Yeah it is VerZion and not VerSion it is not wrong
 var (
-	Verzion = "x.x.x"
+	Verzion = "-dev0.0.0"
 	// Commit     = "x"
 	// CommitDate = "x"
 	// BuiltBy    = "x"
