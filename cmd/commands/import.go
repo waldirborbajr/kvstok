ecf1d7479293a0802cfb3c78b34b407965d4e513
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-11-04 16:02:27 -0300

    update: import routine to check integrity

--- cmd/commands/import.go
+++ cmd/commands/import.go
@@ -25,6 +25,16 @@ var ImpCmd = &cobra.Command{
 	Run:     impVal,
 }
 
+func isEquals(param1 string, param2 string) bool {
+	bret := true
+
+	if param1 != param2 {
+		bret = false
+	}
+
+	return bret
+}
+
 func impVal(cmd *cobra.Command, args []string) {
 	var dataResult map[string]string
 
@@ -40,28 +50,33 @@ func impVal(cmd *cobra.Command, args []string) {
 	currentHash := kvpath.GenHash(configFile)
 	storedHash := []byte(file)
 
-	fmt.Printf("current: %s \n stored: %s \n", currentHash, storedHash)
+	// fmt.Printf("current: %s \n stored: %s \n", currentHash, storedHash)
 
-	if currentHash != string(storedHash) {
+	areEquals := isEquals(currentHash, string(storedHash))
+
+	if !areEquals {
 		log.Fatal("JSON export key corrupted Hash code are not the same.")
 	}
 
-	// Import JSON after integrity check
-	file, err = ioutil.ReadFile(configFile)
-	if err != nil {
-		log.Fatal(err)
-	}
+	if areEquals {
+		// Import JSON after integrity check
+		file, err = ioutil.ReadFile(configFile)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	json.Unmarshal([]byte(file), &dataResult)
+		json.Unmarshal([]byte(file), &dataResult)
 
-	for key, value := range dataResult {
-		if err := database.DB.Update(
-			func(tx *nutsdb.Tx) error {
-				key := []byte(key)
-				val := []byte(value)
-				return tx.Put(database.Bucket, key, val, 0)
-			}); err != nil {
-			fmt.Printf("Error saving value: %s\n", err.Error())
+		for key, value := range dataResult {
+			if err := database.DB.Update(
+				func(tx *nutsdb.Tx) error {
+					key := []byte(key)
+					val := []byte(value)
+					return tx.Put(database.Bucket, key, val, 0)
+				}); err != nil {
+				fmt.Printf("Error saving value: %s\n", err.Error())
+			}
 		}
 	}
+
 }
