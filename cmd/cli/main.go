acfcaf8066e8629d97fec39446aaf68767e502d4
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-11-04 22:06:02 -0300

    chore: implement RSA pub/priv key file

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,48 @@
 package main
 
-import "github.com/waldirborbajr/kvstok/cmd"
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/waldirborbajr/kvstok/cmd"
+	"github.com/waldirborbajr/kvstok/internal/kvpath"
+)
+
+var hasPub = true
+var hasPriv = true
+var privateKey rsa.PrivateKey
+var err error
 
 func main() {
+
+	home := kvpath.GetKVHomeDir()
+
+	pub := home + "/.config/kvstok.pub"
+	priv := home + "/.config/kvstok.priv"
+
+	if _, err := os.Stat(pub); err != nil {
+		hasPub = false
+	}
+
+	if _, err := os.Stat(priv); err != nil {
+		hasPriv = false
+	}
+
+	// Generete new PRIV/PUB RSA Key
+	if !hasPub && !hasPriv {
+		privateKey, err = rsa.GenerateKey(rand.Reader, 4096)
+		if err != nil {
+			log.Fatal("Error GenerateKey: ", err.Error())
+		}
+	}
+
+	publicKey := privateKey.PublicKey
+
+	_ = ioutil.WriteFile(pub, []byte(publicKey), 0o644)
+	_ = ioutil.WriteFile(priv, []byte(privateKey), 0o644)
+
 	cmd.Execute()
 }
