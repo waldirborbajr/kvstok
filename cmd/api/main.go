5b077381a1cbfadfc90437573068450dccbdb73a
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-11-14 11:10:49 -0300

    chore: adjusts to cloud

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -18,20 +17,12 @@ func initServer() {
 		WriteTimeout: 5 * time.Second,
 	}
 
-	server = nil
-
-	if server != nil {
-		log.Println("Starting KVStoK server")
-		err := server.ListenAndServe()
-		log.Println("HTTP server stopped: ", err)
-	} else {
-		log.Println("HTTP server temporarily disabled")
+	log.Printf("Starting KVStoK Server on %s. Press CTRL-C to exit.", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Printf("HTTP server stopped: %s", err.Error())
 	}
 
-	log.Println("KVStoK Server is now running. Press CTRL-C to exit.")
-
 	if server != nil {
-
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 
@@ -43,9 +34,6 @@ func initServer() {
 }
 
 func main() {
-
-	fmt.Println("Starting KVStoK server")
-
 	ch := make(chan struct{})
 	go initServer()
 	<-ch
