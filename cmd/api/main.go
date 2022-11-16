f78c953fbe1efe5b0ca04270b1ae09a4d419e66a
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2022-11-16 10:35:16 -0300

    chore: crating routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,25 +2,57 @@ package main
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io"
 	"log"
+	"net"
 	"net/http"
+	"os"
 	"time"
 )
 
+const keyServerAddr = "serverAddr"
+
 type serverHandler struct{}
+
+func (h serverHandler) KVStoKHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("KVStoK"))
+}
 
 func initServer() {
 
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/getkv", getkvHandler)
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
 	server := &http.Server{
 		Addr:         ":9630",
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
+		Handler:      mux,
+		BaseContext: func(l net.Listener) context.Context {
+			ctx = context.WithValue(ctx, keyServerAddr, l.Addr().String())
+			return ctx
+		},
 	}
 
 	log.Printf("Starting KVStoK Server on %s. Press CTRL-C to exit.", server.Addr)
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server stopped: %s", err.Error())
-	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			log.Printf("Server is closed\n")
+		} else if err != nil {
+			fmt.Printf("Error starting server: %s\n", err.Error())
+			os.Exit(1)
+		}
+		cancelCtx()
+	}()
+	<-ctx.Done()
 
 	if server != nil {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -30,12 +62,22 @@ func initServer() {
 			log.Println("Could not shutdown HTTP KVStoK server: ", err)
 		}
 	}
+}
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got / request\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "This is my website!\n")
+}
+
+func getkvHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	fmt.Printf("%s: got /getkv request\n", ctx.Value(keyServerAddr))
+	io.WriteString(w, "Hello, HTTP!\n")
 }
 
 func main() {
-	ch := make(chan struct{})
-	go initServer()
-	<-ch
-
+	initServer()
 }
