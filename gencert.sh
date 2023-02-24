b87bf2056f5a94b772fd749be12cd4c5030e6a02
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-02-24 17:09:37 -0300

    chore: gencert fix

--- gencert.sh
+++ gencert.sh
@@ -1,3 +1,5 @@
-openssl req -x509 -newkey rsa:4096 -nodes \
+openssl req -x509 -newkey rsa:8102 -nodes \
           -out ./certs/kvcert.pem \
-          -keyout ./certs/kvkey.pem -days 365
+          -keyout ./certs/kvkey.pem -days 365 \
+          -subj "/C=BR/ST=Curitiba/L=Parana/O=B\+ Technology/CN=kvstok.com"
+
