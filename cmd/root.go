98af37ee055296ffb291de7d3e986c5ea8467ef8
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-04-06 11:36:48 -0300

    chore(fix): typos -> error:  should be

--- cmd/root.go
+++ cmd/root.go
@@ -27,7 +27,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	must.Must(rootCmd.Execute(), "Excute() on parsing commands.")
+	must.Must(rootCmd.Execute(), "Execute() on parsing commands.")
 }
 
 // before release 0.2.0 database must be moved to ~/.config/kvstok
