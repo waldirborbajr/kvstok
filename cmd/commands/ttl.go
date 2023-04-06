6824ce07ecfdaa0df1b70708b9e24754f6e3194b
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-04-06 11:22:02 -0300

    chore(add): new feature add time to temporary key (TTL)

---
+++ cmd/commands/ttl.go
@@ -0,0 +1,63 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+
+	"github.com/nutsdb/nutsdb"
+	"github.com/spf13/cobra"
+	"github.com/waldirborbajr/kvstok/internal/database"
+	"github.com/waldirborbajr/kvstok/internal/must"
+)
+
+// AddCmd represents the addkv command
+var TtlCmd = &cobra.Command{
+	Use:     "{t}tladdkv [KEY] [VALUE] [TIME_TO_LIVE_IN_MINUTES]",
+	Short:   "Add a key with time to be live. Default 1 minute.",
+	Long:    ``,
+	Aliases: []string{"t"},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return errors.New("addkv requires at least two parameters [key] and [value] the param [ttl] it is optional, the default value it is 1 minute. Please try it again")
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		// err := database.DB.Update(
+		// 	func(tx *nutsdb.Tx) error {
+		// 		key := []byte(args[0])
+		// 		val := []byte(args[1])
+		// 		ttl := uint32(60)
+		//
+		// 		param_time := string([]byte(args[2]))
+		// 		if len(param_time) != 0 {
+		// 			temp_ttl, _ := strconv.ParseUint(param_time, 10, 32)
+		// 			ttl = uint32(temp_ttl) * 60
+		// 		}
+		//
+		// 		fmt.Println(ttl)
+		//
+		// 		return tx.Put(database.Bucket, key, val, ttl)
+		// 	})
+		//
+		// must.Must(err)
+
+		if err := database.DB.Update(
+			func(tx *nutsdb.Tx) error {
+				key := []byte(args[0])
+				val := []byte(args[1])
+				ttl := uint32(60)
+
+				if len(args) == 3 {
+					temp_ttl, err := strconv.ParseUint(string([]byte(args[2])), 10, 32)
+					must.Must(err, "Third param must be a number.")
+					ttl = uint32(temp_ttl) * 60
+				}
+
+				return tx.Put(database.Bucket, key, val, ttl)
+			}); err != nil {
+			fmt.Printf("Error saving value: %s\n", err.Error())
+		}
+	},
+}
