e37a0eae0512735669fd8d28730c25cccbf9d953
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-04 15:44:11 -0300

    Update kvgetpath.go

--- internal/kvpath/kvgetpath.go
+++ internal/kvpath/kvgetpath.go
@@ -1,3 +1,4 @@
+// Package kvpath provides utilities for resolving file system paths used by kvstok.
 package kvpath
 
 import (
@@ -10,23 +11,21 @@ import (
 	"github.com/waldirborbajr/kvstok/internal/must"
 )
 
-// Get current path and returns
+// GetKVPath returns the path of the current executable.
 func GetKVPath() string {
 	pwd, err := os.Executable()
 	must.Must(err, "GetKVPath() - getting current path.")
-
 	return pwd
 }
 
-// Get $HOME path of user and returns
+// GetKVHomeDir returns the $HOME directory of the current user.
 func GetKVHomeDir() string {
 	home, err := os.UserHomeDir()
 	must.Must(err, "GetKVHomeDir() - getting $HOME path.")
-
 	return home
 }
 
-// Generate HASH of a given file
+// GenHash generates a SHA-256 hash of the given file and returns it as a hex string.
 func GenHash(filename string) string {
 	f, err := os.Open(filename)
 	must.Must(err, "GenHash() - generating Hashcode")
@@ -35,7 +34,6 @@ func GenHash(filename string) string {
 			log.Printf("Error closing file: %s\n", err)
 		}
 	}()
-
 	hasher := sha256.New()
 	if _, err := io.Copy(hasher, f); err != nil {
 		log.Fatal(err)
