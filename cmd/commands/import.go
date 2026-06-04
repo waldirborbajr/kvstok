fcd892069257e7f076570016c1cc7ea863e02cef
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-04 16:13:18 -0300

    Update import.go

--- cmd/commands/import.go
+++ cmd/commands/import.go
@@ -19,52 +19,31 @@ var ImpCmd = &cobra.Command{
 	Aliases: []string{"importkv", "i"},
 	Run: func(cmd *cobra.Command, args []string) {
 		var dataResult map[string]string
-
 		configFile := kvpath.GetKVHomeDir() + "/.config/kvstok/kvstok.json"
 		configHash := kvpath.GetKVHomeDir() + "/.config/kvstok/kvstok.hash"
 
 		// Check export file integrity
 		file, err := os.ReadFile(configHash)
 		must.Must(err, "ImpCmd() - oops! Huston, we have a problem importing keys.")
-
 		currentHash := kvpath.GenHash(configFile)
-		storedHash := []byte(file)
-
-		// areEquals := isEquals(currentHash, string(storedHash))
+		storedHash := file
 		areEquals := currentHash == string(storedHash)
-
 		if !areEquals {
-
 			fmt.Fprintf(os.Stderr, "JSON export key corrupted. Hashcode are not the same.")
 			os.Exit(1)
 		}
-
 		if areEquals {
 			// Import JSON after integrity check
 			file, err = os.ReadFile(configFile)
 			must.Must(err, "ImpCmd() - oops! Huston, we have a problem integrity broken.")
-
-			err = json.Unmarshal([]byte(file), &dataResult)
+			err = json.Unmarshal(file, &dataResult)
 			must.Must(err, "ImpCmd() - failed to parse JSON file")
-
 			store, err := database.GetStore()
 			must.Must(err, "ImpCmd() - failed to open store")
-
 			for key, value := range dataResult {
 				must.Must(store.Put(key, value, 0, nil), "ImpCmd() - oops! Huston, we have a problem importing keys")
 			}
 		}
-
 		fmt.Printf("Keys imported successfully.")
 	},
 }
-
-// func isEquals(param1 string, param2 string) bool {
-// 	bret := true
-
-// 	if param1 != param2 {
-// 		bret = false
-// 	}
-
-// 	return bret
-// }
