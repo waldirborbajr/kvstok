f9a2e0dda32e2507b6579a4651997cb42d922657
BORBA JR, W - PU5MHL <wborbajr@gmail.com>
2026-06-04 17:17:34 -0300

    Update ttl.go

--- cmd/commands/ttl.go
+++ cmd/commands/ttl.go
@@ -9,29 +9,27 @@ import (
 	"github.com/waldirborbajr/kvstok/internal/must"
 )
 
-// TtlCmd represents the ttl command
-var TtlCmd = &cobra.Command{
+// TTLCmd represents the ttl command
+var TTLCmd = &cobra.Command{
 	Use:     "ttl [KEY] [VALUE] [TIME_TO_LIVE_IN_MINUTES]",
 	Short:   "Add a key with time to be live. Default 1 minute.",
 	Long:    ``,
 	Aliases: []string{"ttladdkv", "t"},
-	Args: func(cmd *cobra.Command, args []string) error {
+	Args: func(_ *cobra.Command, args []string) error {
 		if len(args) < 2 {
 			return errors.New("addkv requires at least two parameters [key] and [value] the param [ttl] it is optional, the default value it is 1 minute. Please try it again")
 		}
 		return nil
 	},
-	Run: func(cmd *cobra.Command, args []string) {
+	Run: func(_ *cobra.Command, args []string) {
 		store, err := database.GetStore()
 		must.Must(err, "TTLCmd() - failed to open store")
-
 		ttl := uint32(60)
 		if len(args) == 3 {
 			tempTTL, err := strconv.ParseUint(args[2], 10, 32)
 			must.Must(err, "Third parameter must be a number.")
 			ttl = uint32(tempTTL) * 60
 		}
-
 		must.Must(store.Put(args[0], args[1], ttl, nil), "TTLCmd() - Houston, we have a problem adding or updating the key.")
 	},
 }
